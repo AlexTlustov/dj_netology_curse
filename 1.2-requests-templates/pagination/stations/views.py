c6cc6116e76a818222e9dc46e76be35d8d7a90a0
Aleksandr Tlustov <alex.tlustov@yandex.ru>
2023-08-08 11:01:36 +0300

    feat: Pagi and Recipes

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-
+from django.conf import settings
+from django.core.paginator import Paginator
+import csv
 
 def index(request):
     return redirect(reverse('bus_stations'))
-
-
+    
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
-
+    path_bus_stations = settings.BUS_STATION_CSV
+    with open(path_bus_stations, 'r', encoding='utf-8') as file:
+        csv_data = csv.reader(file)
+        bus_stations = [row for row in csv_data]
+    paginator = Paginator(bus_stations, 10)
+    page_number = int(request.GET.get('page', 1))
+    page = paginator.get_page(page_number)
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': bus_stations,
+        'page': page,
     }
     return render(request, 'stations/index.html', context)
