45c420a34f52512a397c51b4bfbfd626f78049b1
Aleksandr Tlustov <alex.tlustov@yandex.ru>
2023-08-21 09:49:31 +0300

    feat: Last change: Add sort

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -8,6 +8,15 @@ def index(request):
 def show_catalog(request):
     template = 'catalog.html'
     phones = Phone.objects.all()
+    name = request.GET.get('sort')
+    min_price = request.GET.get('sort', 0) 
+    max_price = request.GET.get('sort', 0) 
+    if min_price == 'min_price':
+        phones = phones.order_by('price')
+    if max_price == 'max_price':
+        phones = phones.order_by('-price')
+    if name == 'name':
+        phones = phones.order_by('name')
     context = {'phones': phones}
     return render(request, template, context)
 
